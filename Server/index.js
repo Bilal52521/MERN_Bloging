bb8624d0d4a3e305e1ebf2630c2f1b0f94858310
Hazrat Bilal <hazratbilal52521@gmail.com>
2024-12-09 15:30:37 +0500

    Create and run server

--- Server/index.js
+++ Server/index.js
@@ -1,28 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-
-// let arr = [1 ,2 ,3 ,4 ,5];
-
-// for (let index = 0; index < arr.length; index++) {
-//   console.log(arr[index])
-// }
-
-
-let num = 10;
-// console.log(num + 5)
-
-if(num =   15){
-  console.log("Num is not found")
-} else {
-  console.log(num)
-}
+import express from "express";
+const app = express();
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
