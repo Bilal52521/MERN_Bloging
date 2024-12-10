a86a07da0b6cee95d5578355aa08e9aff8987742
Hazrat Bilal <hazratbilal52521@gmail.com>
2024-12-10 16:33:55 +0500

    create user model

---
+++ Server/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const usreSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("users", usreSchema);
+
+export default User;
