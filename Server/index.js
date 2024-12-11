6be667695e7692a018042a5afe9adcfbd25d638d
Hazrat Bilal <hazratbilal52521@gmail.com>
2024-12-11 11:53:34 +0500

    create a test api

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import connectDB from "./congif.js";
+import UserRouter from "./routes/user.router.js";
 
 const port = 3000;
+const app = express();
+
+// Database Connection
 connectDB();
 
-
-const app = express();
+app.use("/api", UserRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
