0d7490b70d67041b4e825f80e9172df6c9d88ecc
Hazrat Bilal <hazratbilal52521@gmail.com>
2025-01-15 12:11:27 +0500

    create sign in api route

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptpass from "bcrypt";
 import { errHandler } from "../utils/errMessage.js";
+import jwt from "jsonwebtoken";
 
 export const sign_up = async (req, res, next) => {
   try {
@@ -30,6 +31,34 @@ export const sign_up = async (req, res, next) => {
     res.status(201).json({ message: "User successfully created!" });
   } catch (error) {
     console.error("Error creating user:", error);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errHandler(400, "All fields are rquired!"));
+  }
+
+  try {
+    const userValid = await User.findOne({ email });
+    if (!userValid) {
+      return next(errHandler(404, "User not found!"));
+    }
+    const passwordValid = bcryptpass.compareSync(password, userValid.password);
+
+    if (!passwordValid) {
+      return next(errHandler(400, "Invalid Password!"));
+    }
+
+    const token = jwt.sign({ userId: userValid._id }, process.env.JWT_SECRET);
+
+    const {password : pass , ...rest} = userValid._doc;
+
+    res.status(200).cookie("access_token", token).json(rest);
+  } catch (error) {
     next(error);
   }
 };
--- Server/routes/auth.route.js
+++ Server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { sign_up } from "../controllers/auth.controller.js";
+import { sign_up, signin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/sign_up", sign_up);
+router.post("/signin", signin);
 
 export default router;
